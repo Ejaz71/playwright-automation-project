3957c52cf54abad8a2974d473c00ede5377c78a3
Ejaz71 <ejazchowdhury.buetynwa@gmail.com>
2025-07-08 00:44:24 +0600

    Added positive and negative login tests

---
+++ locators/locators.ts
@@ -0,0 +1,15 @@
+export const locators = {
+  // Login Page Locators        
+    loginField: '#username',    
+    passwordField: '[type="password"]',
+    signInButton: '#signInBtn',     
+    // Error Message Locators       
+    errorMessage: '.alert-danger',
+    // Success Message Locators     
+    successMessage: '.alert-success',
+    // Navigation Locators  
+    homeLink: 'a[href="/angularpractice/shop"]',
+    // Product Locators
+    productCard: '.card',
+}
+
--- tests/UIBasics.spec.ts
+++ tests/UIBasics.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+import {locators} from '../locators/locators';
 
 test('First Playwright Test', async ({ page, browser }) => {
     const context = await browser.newContext();
@@ -11,6 +12,29 @@ test('Navigate to gootle.com', async ({ page }) => {
     await page.goto('https://www.google.com');
     // Check if the title contains "Google"
     const title = await page.title();
-    console.log(`Page title is: ${title}`);
+    //console.log(`Page title is: ${title}`);
     await expect(page).toHaveTitle(title);
+});
+
+test('Login to site with incorrect credentials', async ({ page }) => {
+    await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+    const loginField = await page.locator('#username').fill('incorrectUser');
+    const passwordField = await page.locator('[type="password"]').fill('incorrectPassword');
+    await page.locator('#signInBtn').click();
+    // Check for error message  
+    const errorMessage = await page.locator('.alert-danger').textContent();
+    //console.log(`Error message is: ${errorMessage}`);         
+    await expect(page.locator('.alert-danger')).toHaveText('Incorrect username/password.');
+    // Check if the error message is visible    
+    await expect(page.locator('.alert-danger')).toBeVisible();         
+});
+
+test('Login to site with correct credentials', async ({ page }) => {
+    await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+    const loginField = await page.locator(locators.loginField).fill('rahulshettyacademy');
+    const passwordField = await page.locator(locators.passwordField).fill('learning');
+    await page.locator(locators.signInButton).click();
+    // verify user has logged in to https://rahulshettyacademy.com/angularpractice/shop
+    await expect(page).toHaveURL('https://rahulshettyacademy.com/angularpractice/shop');  
+             
 });
